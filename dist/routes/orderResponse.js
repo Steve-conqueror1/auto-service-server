f05aceddaa2fd2f1bd325a15111a784c8c1aa22d
Stephen Kilonzo <stephenkilonzo.scholar@gmail.com>
2023-05-08 19:09:20 +0300

    Autoservice: removed dist

--- dist/routes/orderResponse.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const orderResponse_1 = require("../controllers/orderResponse");
-const router = (0, express_1.Router)();
-router.post('/', orderResponse_1.createOrderResponse);
-exports.default = router;
-//# sourceMappingURL=orderResponse.js.map
